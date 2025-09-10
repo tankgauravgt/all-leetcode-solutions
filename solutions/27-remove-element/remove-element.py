4de145d3cdf413d1adaa6bb3cbe81bd5467e0bcf
Gaurav Tank <code.tankgauravgt@gmail.com>
2025-09-11 02:43:32 +0530

    Time: 0 ms (100.00%) | Memory: 18 MB (11.16%) - LeetSync

---
+++ solutions/27-remove-element/remove-element.py
@@ -0,0 +1,12 @@
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        N = len(nums)
+
+        write_head = 0
+        for read_head in range(N):
+            if nums[read_head] == val:
+                continue
+            nums[write_head] = nums[read_head]
+            write_head = write_head + 1
+
+        return write_head
