bf0d8f4d8bc5807d7bf166c9d6c75a2aae8084eb
Gaurav Tank <code.tankgauravgt@gmail.com>
2025-09-24 23:26:14 +0530

    Time: 1609 ms (16.40%) | Memory: 601.8 MB (5.06%) - LeetSync

--- solutions/123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.py
+++ solutions/123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.py
@@ -1,24 +1,14 @@
-from functools import cache
-from typing import List
-
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         N = len(prices)
 
-        MAX_TRANSACTIONS = 4
-
         @cache
-        def solve(day_index: int, transaction_id: int) -> int:
-            if day_index == N or transaction_id == MAX_TRANSACTIONS:
+        def rec(sx, n_tx):
+            if sx == N or n_tx == 4:
                 return 0
-            
-            do_nothing_profit = solve(day_index + 1, transaction_id)
-            
-            if transaction_id % 2 == 0:
-                action_profit = -prices[day_index] + solve(day_index + 1, transaction_id + 1)
-            else:
-                action_profit = prices[day_index] + solve(day_index + 1, transaction_id + 1)
-            
-            return max(do_nothing_profit, action_profit)
-
-        return solve(0, 0)
+            return max(
+                rec(1 + sx, n_tx),
+                (2 * (n_tx % 2) - 1) * prices[sx] + rec(1 + sx, n_tx + 1)
+            )
+        
+        return rec(0, 0)
